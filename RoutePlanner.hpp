9a13cc163d4716b5e656da33f2efa89230576ec8
shickmanIV <113073744+shickmanIV@users.noreply.github.com>
2023-02-18 13:40:36 -0800

    Created RoutePlanner.hpp
    Created a header file for the Star and DataParser classes.

---
+++ RoutePlanner.hpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+class Star
+{
+private:
+  std::string name;
+  double gal_x;
+  double gal_y;
+  double gal_z;
+  double mass_kg;
+  double luminocity;
+public:
+  std::string getName();
+}
+
+class DataParser
+{
+
+}
