a0ae37240d45e3f944720b68fbf8dae5caa83855
JonahChesnut <jonah.chesnut@wsu.edu>
2023-02-18 15:52:30 -0800

    Added comments to header file

--- RoutePlanner.hpp
+++ RoutePlanner.hpp
@@ -22,8 +22,14 @@ struct Node {
 	Node* pNext;
 };
 
+//Finds a route using short jumps to eventually go from start to end
 Node* makeRoute(Star& start, Star& end);
 
+//Makes a node and initializes it with the input data
 Node* makeNode(Star& data);
 
-int insertStarAtFront(Star& list, Star& insert);
+//Inserts a star at the beginning of the list
+int insertStarAtFront(Star** list, Star& insert);
+
+//Prints the list
+void printList(Star& list);
