cbb53f37a41a5c6d9560169615789f7795c8e44d
shickmanIV <samuel.hickman@wsu.edu>
2023-02-18 21:23:46 -0800

    Improved the organization of the struct's template colors.

--- ImageProcessor.cpp
+++ ImageProcessor.cpp
@@ -7,27 +7,36 @@
     
 ImageProcessor::ImageProcessor()
 {
-    white = make_colour(255, 255, 255);
-    gray = make_colour(128, 128, 128);
-    black = make_colour(0, 0, 0);
+    getColorTemplates();
 }
 
-ImageProcessor::ImageProcessor(int width, int height)
+ImageProcessor::ImageProcessor(int pixel_width, int pixel_height)
 {
-    //Create a blank image.
-    bitmap_image newImage(width,height);
+    getColorTemplates();
+
+    //Create an all-black image.
+    bitmap_image newImage(pixel_width, pixel_height);
     newImage.set_all_channels(0, 0, 0);
     image = newImage;
 }
 
 ImageProcessor::ImageProcessor(bitmap_image& newImage)
 {
+    getColorTemplates();
     image = newImage;
 }
 
 ImageProcessor::ImageProcessor(ImageProcessor& copy)
 {
-     image = copy.image;
+    getColorTemplates();
+    image = copy.image;
+}
+
+void ImageProcessor::getColorTemplates()
+{
+    white = make_colour(255, 255, 255);
+    gray = make_colour(128, 128, 128);
+    black = make_colour(0, 0, 0);
 }
 
 //Paints Horizontal and Vertical gridlines, alligning at the origin.
@@ -41,6 +50,8 @@ void ImageProcessor::plotRadialGrid(size_t color)
 {
     //TODO: do this
 }
+
+
 
 //Marks every star location as a colored point on the image.
 void ImageProcessor::plotStarLocations(Star stars[], int numStars, int radius, rgb_t color)
@@ -63,4 +74,25 @@ void ImageProcessor::drawCircleAtPoint(int x, int y, int radius, rgb_t color)
     {
         draw.circle(x, y, i);
     }
+} 
+
+double getDistanceInLightYears(double x1, double y1, double x2, double y2)
+{
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+double getDistance(Star& first, Star& second) {
+    double x2 = 0.0, y2 = 0.0, z2 = 0.0, distance = 0.0;
+
+    x2 = pow(second.gal_x - first.gal_x, 2);
+    y2 = pow(second.gal_y - first.gal_y, 2);
+    z2 = pow(second.gal_z - first.gal_z, 2);
+
+    distance = sqrt(x2 + y2 + z2);
+    return distance;
+}
+
+struct Star getClosestStar(int x, int y, struct Star sortedStars[])
+{
+    
 }
