2f320945f4da3555311c8c69e3a7ce340842c3cf
MMenser <119845585+MMenser@users.noreply.github.com>
2023-02-18 18:10:56 -0800

    Parsing stuff

---
+++ ParseBranch/ParseBranch/functions.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+* Programmer: Mason Menser	                                            *
+* Class: CptS 121				                                        *
+* Programming Assignment:    							                *
+*												                        *
+* Date:   9/26/2022                                                     *
+*                                                                       *
+* Description:															*
+************************************************************************/
+
+
+#include "header.h"
+
+//Function definitions 
